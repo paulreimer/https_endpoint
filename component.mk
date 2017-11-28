ccc30cc4cc5bead7d5e84681dfcbc1e4ff4193bb
Paul Reimer <paul@p-rimes.net>
2017-11-27 16:07:18 -0800

    🚨 Add -Wno-type-limits to CXXFLAGS for compiling flatbuffers idl_parser.cpp

--- component.mk
+++ component.mk
@@ -16,4 +16,4 @@ CXXFLAGS += \
 	-DFLATBUFFERS_NO_ABSOLUTE_PATH_RESOLUTION \
 	-DPICOJSON_USE_INT64=1
 
-flatbuffers/src/idl_parser.o: CXXFLAGS += -Wno-maybe-uninitialized
+flatbuffers/src/idl_parser.o: CXXFLAGS += -Wno-maybe-uninitialized -Wno-type-limits
