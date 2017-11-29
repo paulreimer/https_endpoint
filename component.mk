7d36dd0fa3fed9ef103dbfbf9b181658dd718843
Paul Reimer <paul@p-rimes.net>
2017-11-28 18:18:23 -0800

    🔧 Add make rules for generating src/*.bfbs and src/*_generated.h from src/*.fbs flatbuffers schema, and added src/oidc.fbs and src/oidc.bfbs to COMPONENT_EMBED_TXTFILES

--- component.mk
+++ component.mk
@@ -17,4 +17,25 @@ CXXFLAGS += \
 	-DFLATBUFFERS_NO_ABSOLUTE_PATH_RESOLUTION \
 	-DPICOJSON_USE_INT64=1
 
+# Embed files from as binary data symbols
+# Use COMPONENT_EMBED_TXTFILES instead of COMPONENT_EMBED_FILES
+# So it will be null-terminated
+COMPONENT_EMBED_TXTFILES := \
+	src/oidc.fbs \
+	src/oidc.bfbs
+
+# Output these files to the component path
+$(COMPONENT_PATH)/src/%_generated.h: $(COMPONENT_PATH)/src/%.fbs
+	flatc --cpp -o $(COMPONENT_PATH)/src \
+		--gen-mutable \
+		--gen-object-api \
+		--gen-name-strings \
+		--reflect-types \
+		--reflect-names \
+		$^
+
+# Output these files to the component path
+$(COMPONENT_PATH)/src/%.bfbs: $(COMPONENT_PATH)/src/%.fbs
+	flatc --binary --schema -o $(COMPONENT_PATH)/src $^
+
 flatbuffers/src/idl_parser.o: CXXFLAGS += -Wno-maybe-uninitialized -Wno-type-limits
